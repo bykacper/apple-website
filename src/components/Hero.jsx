5c5af51b79de1f44f25fded9f4bc58ff31c6fb77
Kacper <bykacper@gmail.com>
2025-01-06 12:32:34 +0100

    work on the slider has started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,12 @@ const Hero = () => {
             opacity: 1,
             delay: 1.5
         })
+
+        gsap.to("#cta", {
+            opacity: 1,
+            y: -50,
+            delay: 1.5
+        })
     }, [])
 
     return (
@@ -35,9 +41,14 @@ const Hero = () => {
                 <p id="hero" className="hero-title"> iPhone 15 Pro </p>
                 <div className="md:w-10/12 w-9/12">
                     <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
-                        <source src={videoSrc}/>
+                        <source src={videoSrc} type="video/mp4" />
                     </video>
                 </div>
+            </div>
+
+            <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+                <a href="#highlights" className="btn"> Buy </a>
+                <p className="font-normal text-xl"> From $199/month or $999 </p>
             </div>
         </section>
     )
